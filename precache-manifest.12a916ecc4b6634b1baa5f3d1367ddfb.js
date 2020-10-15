1d9d01d529420a22de3250ffb96e128626f4fafd
NIHAL TIWARI <nihaltiwari28@gmail.com>
2020-10-14 22:59:12 -0700

    Updates

---
+++ precache-manifest.12a916ecc4b6634b1baa5f3d1367ddfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d74fbfb446ccc9fe80592f6997af841",
+    "url": "/crud/index.html"
+  },
+  {
+    "revision": "227d3ea92bc5ce30b18b",
+    "url": "/crud/static/css/main.8bb7bf4a.chunk.css"
+  },
+  {
+    "revision": "082a7568a1aa5920f9c7",
+    "url": "/crud/static/js/2.54590f22.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crud/static/js/2.54590f22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "227d3ea92bc5ce30b18b",
+    "url": "/crud/static/js/main.b3cc5f9d.chunk.js"
+  },
+  {
+    "revision": "3d2107f09fcddc21becb",
+    "url": "/crud/static/js/runtime-main.49a9eee0.js"
+  }
+]);
